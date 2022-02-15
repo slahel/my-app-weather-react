a9963c6a9768698bd1b0470ab121605c7fdc3471
slahel <74063357+slahel@users.noreply.github.com>
2022-02-15 18:37:25 +0100

    5 daily forecast columns

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -14,7 +14,19 @@ export default function WeatherForecast(props) {
     console.log(forecast);
     return (
       <div className="Forecast">
-        <ForecastDay data={forecast[0]} />
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
       </div>
     );
   } else {
